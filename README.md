a6c943568df18a5f404094dd08796e031d5be911
Kristijan Husak <husakkristijan@gmail.com>
2018-04-14 15:31:12 +0200

    Set rank to 500, print issues if any, reorganize code.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # deoplete-phpactor
-Phpactor integration for deoplete
+[Phpactor](https://github.com/phpactor/phpactor) integration for [deoplete.nvim](https://github.com/Shougo/deoplete.nvim)
+
+
+## Installation
+
+Using [vim-plug](https://github.com/junegunn/vim-plug)
+
+```
+Plug 'Shougo/deoplete.nvim'
+Plug 'phpactor/phpactor' ,  {'do': 'composer install', 'for': 'php'}
+Plug 'kristijanhusak/deoplete-phpactor'
+```
--- rplugin/python3/deoplete/source/phpactor.py
+++ rplugin/python3/deoplete/source/phpactor.py
@@ -12,20 +12,19 @@ class Source(Base):
         self.filetypes = ['php']
         self.is_bytepos = True
         self.input_pattern = '\w+|[^. \t]->\w*|\w+::\w*|\\\\'
-        self.rank = 9999
+        self.rank = 500
         self.max_pattern_length = -1
         self.matchers = ['matcher_full_fuzzy']
-        self._files = {}
 
     def get_complete_position(self, context):
         line = self.vim.eval('getline(".")')
         start = self.vim.eval('col(".")')
-        triggers = ["->", "::"]
+        triggers = ['->', '::']
         while start > 0:
-            if line[start-1:start] == "$":
+            if line[start - 1:start] == '$':
                 return start
 
-            if line[start-2:start] in triggers:
+            if line[start - 2:start] in triggers:
                 return start
 
             start -= 1
@@ -33,45 +32,62 @@ class Source(Base):
 
     def gather_candidates(self, context):
         self._phpactor = self.vim.eval(
-            r"""globpath(&rtp, 'bin/phpactor', 1)""")
+            r"""globpath(&rtp, 'bin/phpactor', 1)"""
+        )
 
         if not self._phpactor:
             self.print_error(
                 'phpactor not found,'
-                ' please install https://github.com/phpactor/phpactor')
-        candidates = []
-        offset = int(
-            self.vim.eval('line2byte(line("."))')) + int(
-                self.vim.eval('col(".")')) - 2 + len(
-                    context['complete_str'])
-        args = ['php',
-                self._phpactor,
-                'rpc',
-                '--working-dir=%s' % self.vim.eval('getcwd()')]
-        proc = subprocess.Popen(args=args,
-                                stdin=subprocess.PIPE,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
+                ' please install https://github.com/phpactor/phpactor'
+            )
 
-        source = self.vim.eval("join(getline(1, '$'), '\n')")
-        data = json.dumps(
-            {"action": "complete",
-             "parameters":
-             {"source": source, "offset": offset}})
+        candidates = []
+
+        line_offset = int(self.vim.eval('line2byte(line("."))'))
+        column_offset = int(self.vim.eval('col(".")')) - 2
+        offset = line_offset + column_offset + len(context['complete_str'])
+
+        args = [
+            'php',
+            self._phpactor,
+            'rpc',
+            '--working-dir=%s' % self.vim.eval('getcwd()')
+        ]
+
+        proc = subprocess.Popen(
+            args=args,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+
+        source = self.vim.eval('join(getline(1, "$"), "\n")')
+        data = json.dumps({
+            'action': 'complete',
+            'parameters': {'source': source, 'offset': offset}
+        })
         result, errs = proc.communicate(data.encode('utf-8'))
 
         errs = errs.decode()
         if errs:
             return self.print_error(errs)
 
-        result = result.decode()
-        result = json.loads(result)
+        result = json.loads(result.decode())
+        result = result['parameters']['value']
 
-        suggestions = result['parameters']['value']['suggestions']
-        if result['action'] == 'return' and len(suggestions) > 0:
-            for item in suggestions:
-                candidates.append({'word': item['name'],
-                                   'menu': item['info'],
-                                   'kind': item['type']})
+        if len(result['issues']) > 0:
+            self.vim.call(
+                'deoplete#util#print_error',
+                ', '.join(result['issues']),
+                'deoplete-phpactor'
+            )
+
+        if len(result['suggestions']) > 0:
+            for suggestion in result['suggestions']:
+                candidates.append({
+                    'word': suggestion['name'],
+                    'menu': suggestion['info'],
+                    'kind': suggestion['type']
+                })
 
         return candidates
